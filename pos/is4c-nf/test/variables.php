57a9aa66c0f197d7b73252e9962d5db69a2ff5e2
Joel Brock <joelbrock@gmail.com>
2014-01-25 20:23:55 -0800

    variables script for debugging/development

--- pos/is4c-nf/test/variables.php
+++ pos/is4c-nf/test/variables.php
@@ -1,9 +1,16 @@
 <?php
 
 include('../lib/AutoLoader.php');
-
-echo '<ul>';
-
-echo '</ul>';
+echo "<h3>Current PHP \$_SESSION Variables</h3>";
+foreach($_SESSION as $key => $val) {
+	echo $key.": ";
+	if(is_array($val) && isset($val)){
+		print_r($val);
+		echo "<br />";
+	} else {
+		echo $val."<br />";
+	}
+}
 
 ?>
+
