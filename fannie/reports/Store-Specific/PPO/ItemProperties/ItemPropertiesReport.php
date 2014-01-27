8f0a64617ee899a5d3a8c0087d8e01a0e2aaf3b4
Joel Brock <joelbrock@gmail.com>
2014-01-26 17:21:45 -0800

    itemprops report IN PROGRESS

--- fannie/reports/Store-Specific/PPO/ItemProperties/ItemPropertiesReport.php
+++ fannie/reports/Store-Specific/PPO/ItemProperties/ItemPropertiesReport.php
@@ -1,16 +1,26 @@
 <?php
+
+include('../../../../config.php');
+include($FANNIE_ROOT.'classlib2.0/FannieAPI.php');
+
 $page_title = 'Fannie - Reporting';
 $header = 'Item Properties Report';
+class ItemPropertiesReport extends FannieReportPage
+{
+    protected $title = "Fannie : Item Properties";
+    protected $header = "Item Properties Report";
+    protected $report_headers = array('Date','UPC','Description','Qty','$');
+    protected $required_fields = array('year1', 'year2');
 
-// to: miguel
-// RE: sales reporting in the dept. sales report (and all values reporting on sales data):  Presently the way i have designed this feature, the values reporting on sales is not "historical".  Meaning values data (local, org., etc.) is tied to the item UPC, not some field in the tlogs.  So sales data will reflect an items CURRENT values, not the values they held when they were sold.  I did it this way so that once buyers had entered most of the values for their products, you could then go back and look at historical sales data using the new values reporting.  This does mean however that if values data for items in a report have changed, the values-based sales data will CHANGE depending on when a report is run!  I suspect this may only be an issue in the Produce dept. (and poss. bulk).  ex.  Summertime:  Lettuce is Local for all of July.  The Report will
+
+	function fetch_report_data() {
 
 
-if ($_POST['submit']) {
-	require_once '../define.conf';
-	include '../src/functions.php';
-	include 'reportFunctions.php';
-	include '../src/header.php';
+//if ($_REQUEST['submit']) {
+//	require_once '../../../../config.conf';
+//	include '../src/functions.php';
+//	include 'reportFunctions.php';
+//	include '../../../../src/header.php';
 	echo '<script type="text/javascript" language="Javascript" src="http://code.highcharts.com/highcharts.js"></script>';
 	foreach ($_POST AS $key => $value) {
 		$$key = $value;
@@ -74,13 +84,13 @@ if ($_POST['submit']) {
 	
 	// debug_p($_REQUEST, "all the data coming in");
 	
-	include '../src/footer.php'; 	
-	
-} else {
+	//include '../src/footer.php'; 	
+	}
+//} else {
 
-	include '../src/header.php';
-
-	echo "<form action=\"itemProperties2.php\" method=\"POST\" target=\"_blank\">\n
+//	include '../../../../src/header.php';
+	function form_content() {
+		echo "<form action=\"itemProperties.php\" method=\"POST\" target=\"_blank\">\n
 		<table>\n<tr>
 		<td>Date Start:</td>\n
 		<td><div class=\"date\"><p><input type=\"text\" name=\"date1\" class=\"datepicker\" />&nbsp;&nbsp;*</p></div></td>\n
@@ -94,7 +104,9 @@ if ($_POST['submit']) {
 
 
 
-	include '../src/footer.php'; 
+		include $FANNIE_ROOT.'src/footer.php'; 
+	}
+//}
 }
 
 ?>
