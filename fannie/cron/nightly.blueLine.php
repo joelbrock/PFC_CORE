f71ffdbbf141ace0fa6aeddc514849b79806d5ef
Joel Brock <joelbrock@gmail.com>
2014-01-27 21:50:22 -0800

    cron task for adding memtype to blueline

---
+++ fannie/cron/nightly.blueLine.php
@@ -0,0 +1,48 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2014 Whole Foods Co-op
+
+    This file is part of Fannie.
+
+    Fannie is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Fannie is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+
+/* HELP
+
+   nightly.blueLine.php
+
+   Update custdata.blueline add memtype to the end
+*/
+
+include('../config.php');
+include($FANNIE_ROOT.'src/SQLManager.php');
+include($FANNIE_ROOT.'src/cron_msg.php');
+
+set_time_limit(0);
+
+$sql = new SQLManager($FANNIE_SERVER,$FANNIE_SERVER_DBMS,$FANNIE_OP_DB,
+		$FANNIE_SERVER_USER,$FANNIE_SERVER_PW);
+
+$TRANS = ($FANNIE_SERVER_DBMS == "MSSQL") ? $FANNIE_TRANS_DB.".dbo." : $FANNIE_TRANS_DB.".";
+
+$upQ = "UPDATE custdata AS c, memtype AS m
+	SET c.blueLine = CONCAT(c.blueLine,' - ',m.memDesc)
+	WHERE c.memtype = m.memtype AND c.memtype IN (1,2,3,4,5)";
+$sql->query($upQ);
+
+
+?>
